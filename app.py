fc58b48a26a13426cd02abfef15ecfac0f2f92a1
audibleblink <virtualsnyper"gmail.com>
2014-10-14 22:41:50 +0000

    setup gpio lib

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 import httplib2
+import time
 import simplejson
-# import RPi.GPIO as GPIO
-
-# GPIO.setmode(GPIO.BOARD)
+import RPi.GPIO as GPIO
 
 settings = {
-  "url": "https://raw.githubusercontent.com/audibleblink/mock-data/3a2fcca57d691501f2a108a26e795722de58c579/binary/true.json"
+  "url": "https://raw.githubusercontent.com/audibleblink/mock-data/master/binary/true.json",
+  "pin": 7 
 }
 
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(settings["pin"], GPIO.OUT)
+
+
 def status(url):
-  response, content = httplib2.Http().request(url)
+  response, content = httplib2.Http(disable_ssl_certificate_validation=True).request(url)
   return simplejson.loads(content)
   
 def unread(response):
   return True if response["unread"] else False
 
 def turn_on_for_what():
+  GPIO.output( settings["pin"], True )
+  time.sleep(.5)
+  GPIO.output( settings["pin"] , False )
   print "Light ON!"
 
-if unread( status(settings["url"]) ):
-  turn_on_for_what()
+while True:
+  if unread( status(settings["url"]) ):
+    turn_on_for_what()
+    time.sleep(5)
+
+
+GPIO.cleanup()
 
