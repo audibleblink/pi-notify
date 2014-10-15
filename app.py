ca94a0069be865a451140e60b580e03cb8928d40
audibleblink <virtualsnyper@gmail.com>
2014-10-14 23:55:24 -0400

    create #main function and pass it configs

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
-import httplib2
 import time
+import httplib2
 import simplejson
 import RPi.GPIO as GPIO
 
-settings = {
-  "url": "https://raw.githubusercontent.com/audibleblink/mock-data/master/binary/true.json",
+
+config = {
+  "url": "http://bit.ly/1DaR7WV",
   "pin": 7 
 }
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(settings["pin"], GPIO.OUT)
 
-def status(url):
-  client = httplib2.Http(disable_ssl_certificate_validation=True)
-  response, content = client.request(url)
+def status(args):
+  client = httplib2.Http(disable_ssl_certificate_validation = True)
+  response, content = client.request(args["url"])
   return simplejson.loads(content)
   
 def unread(response):
   return True if response["unread"] else False
 
-def turn_on_for_what(pin):
-  GPIO.output(pin, True)
+def turn_on_for_what(args):
+  GPIO.output(args["pin"], True)
   time.sleep(.5)
-  GPIO.output(pin , False)
+  GPIO.output(args["pin"] , False)
 
+def main(args):
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(args["pin"], GPIO.OUT)
 
-pull_requests = status(settings["url"])
-while True:
-  if unread(pull_requests):
-    turn_on_for_what(settings["pin"])
-    time.sleep(5)
+  pull_requests = status(args)
 
+  while True:
+    if unread(pull_requests):
+      turn_on_for_what(args)
+      time.sleep(2)
+
+main(config)
 
 GPIO.cleanup()
 
