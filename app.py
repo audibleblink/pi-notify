9add6669ef96fc7af55964353924313dd40adc14
audibleblink <virtualsnyper@gmail.com>
2014-10-15 13:55:40 -0400

    indents

--- app.py
+++ app.py
@@ -5,36 +5,38 @@ import RPi.GPIO as GPIO
 
 
 config = {
-  "url": "http://bit.ly/1DaR7WV",
-  "pin": 7 
+    "url": "http://bit.ly/1DaR7WV",
+    "pin": 7 
 }
 
 
 def status(args):
-  client = httplib2.Http(disable_ssl_certificate_validation = True)
-  response, content = client.request(args["url"])
-  return simplejson.loads(content)
+    client = httplib2.Http(disable_ssl_certificate_validation = True)
+    response, content = client.request(args["url"])
+    return simplejson.loads(content)
   
 def unread(response):
-  return True if response["unread"] else False
+    return True if response["unread"] else False
 
 def turn_on_for_what(args):
-  GPIO.output(args["pin"], True)
-  time.sleep(.5)
-  GPIO.output(args["pin"] , False)
+    GPIO.output(args["pin"], True)
+    time.sleep(.5)
+    GPIO.output(args["pin"] , False)
+
+
 
 def main(args):
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(args["pin"], GPIO.OUT)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(args["pin"], GPIO.OUT)
 
-  pull_requests = status(args)
+    pull_requests = status(args)
 
-  while True:
-    if unread(pull_requests):
-      turn_on_for_what(args)
-      time.sleep(2)
+    while True:
+        if unread(pull_requests):
+            turn_on_for_what(args)
+            time.sleep(2)
 
-main(config)
+            main(config)
 
-GPIO.cleanup()
+# GPIO.cleanup()
 
