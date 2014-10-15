f68244e75a1912e5190b8029ebc68e5001aeb6ea
audibleblink <virtualsnyper@gmail.com>
2014-10-15 17:03:23 -0400

    spacing

--- app.py
+++ app.py
@@ -14,15 +14,16 @@ def status(args):
     client = httplib2.Http(disable_ssl_certificate_validation = True)
     response, content = client.request(args["url"])
     return simplejson.loads(content)
+
   
 def unread(response):
     return True if response["unread"] else False
+
 
 def turn_on_for_what(args):
     GPIO.output(args["pin"], True)
     time.sleep(.5)
     GPIO.output(args["pin"] , False)
-
 
 
 def main(args):
@@ -32,11 +33,12 @@ def main(args):
     pull_requests = status(args)
 
     while True:
+
         if unread(pull_requests):
             turn_on_for_what(args)
             time.sleep(2)
 
-            main(config)
+main(config)
 
 # GPIO.cleanup()
 
